9272c19b14457c0aa00266bd2eaa8f1d56c959ae
Frank Carvalho <frank.faculdadeadm@gmail.com>
2024-11-22 14:46:29 -0500

    Projeto imersao-backend

---
+++ src/routes/postsRoutes.js
@@ -0,0 +1,24 @@
+import express from "express";
+import multer from "multer";
+import { listarPosts, postarNovoPost, uploadImagem, atualizarNovoPost } from "../controllers/postsController.js";
+import cors from "cors"
+
+const corsOpt = {
+    origin: "http://localhost:8000",
+    optionsSuccessStatus: 200
+}
+
+const upload = multer({dest:"./uploads"})
+
+const routes = (app) => {
+    app.use(express.json());
+
+    app.use(cors(corsOpt));
+
+    app.get("/posts", listarPosts);
+    app.post("/posts", postarNovoPost);
+    app.post("/upload", upload.single("imagem"), uploadImagem);
+    app.put("/upload/:id", atualizarNovoPost);
+};
+
+export default routes;
